3411b4c4a27d51dd4eb05e91d2457328cf045474
John Sharp <johns@contentmaster.com>
2019-05-02 17:31:01 +0100

    Add files via upload

---
+++ secretapp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace secretapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step 1: Get a token from the local (URI) Managed Service Identity endpoint, which in turn fetches it from Azure AD
+            var token = GetToken();
+
+            // Step 2: Fetch the secret value from your key vault
+            System.Console.WriteLine(FetchSecretValueFromKeyVault(token));
+        }
+
+        static string GetToken()
+        {
+            WebRequest request = WebRequest.Create("http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net");
+            request.Headers.Add("Metadata", "true");
+            WebResponse response = request.GetResponse();
+            return ParseWebResponse(response, "access_token");
+        }
+
+        static string FetchSecretValueFromKeyVault(string token)
+        {
+            WebRequest kvRequest = WebRequest.Create("https://jpwskeyvault.vault.azure.net/secrets/jabberwocky?api-version=2016-10-01");
+            kvRequest.Headers.Add("Authorization", "Bearer " + token);
+            WebResponse kvResponse = kvRequest.GetResponse();
+            return ParseWebResponse(kvResponse, "value");
+        }
+
+        private static string ParseWebResponse(WebResponse response, string tokenName)
+        {
+            string token = String.Empty;
+            using (Stream stream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                String responseString = reader.ReadToEnd();
+
+                JObject joResponse = JObject.Parse(responseString);
+                JValue ojObject = (JValue)joResponse[tokenName];
+                token = ojObject.Value.ToString();
+            }
+            return token;
+        }
+    }
+}
