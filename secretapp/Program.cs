e74ee2db978cd3324c82dc7d95e02c9025b03fd2
John Sharp <johns@contentmaster.com>
2019-05-03 09:23:48 +0100

    Update Program.cs

--- secretapp/Program.cs
+++ secretapp/Program.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.IO;
-using System.Net;
-using System.Text;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+using System;
+using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.Services.AppAuthentication;
+using System.Threading.Tasks;
 
 namespace secretapp
 {
@@ -12,61 +9,23 @@ namespace secretapp
     {
         static void Main(string[] args)
         {
-            // Step 1: Get a token from the local (URI) Managed Service Identity endpoint, which in turn fetches it from Azure AD
-            //var token = GetToken();
-
-            // Step 2: Fetch the secret value from your key vault
-            //System.Console.WriteLine(FetchSecretValueFromKeyVault(token));
-            
             AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
-
             GetSecretFromKeyVault(azureServiceTokenProvider).Wait();
         }
-
-        static string GetToken()
-        {
-            WebRequest request = WebRequest.Create("http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net");
-            request.Headers.Add("Metadata", "true");
-            WebResponse response = request.GetResponse();
-            return ParseWebResponse(response, "access_token");
-        }
-
-        static string FetchSecretValueFromKeyVault(string token)
-        {
-            WebRequest kvRequest = WebRequest.Create("https://jpwskeyvault.vault.azure.net/secrets/jabberwocky?api-version=2016-10-01");
-            kvRequest.Headers.Add("Authorization", "Bearer " + token);
-            WebResponse kvResponse = kvRequest.GetResponse();
-            return ParseWebResponse(kvResponse, "value");
-        }
-
-        private static string ParseWebResponse(WebResponse response, string tokenName)
-        {
-            string token = String.Empty;
-            using (Stream stream = response.GetResponseStream())
-            {
-                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
-                String responseString = reader.ReadToEnd();
-
-                JObject joResponse = JObject.Parse(responseString);
-                JValue ojObject = (JValue)joResponse[tokenName];
-                token = ojObject.Value.ToString();
-            }
-            return token;
-        }
-        
-        private static void GetSecretFromKeyVault(AzureServiceTokenProvider azureServiceTokenProvider)
+       
+        private static async Task GetSecretFromKeyVault(AzureServiceTokenProvider azureServiceTokenProvider)
         {
             KeyVaultClient keyVaultClient =
                 new KeyVaultClient(
                     new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
 
             var keyVaultName = "jpwskeyvault";
-            var secret = "jabberwocky";
+            var keyVaultSecretName = "jabberwocky";
 
             try
             {
                 var secret = await keyVaultClient
-                    .GetSecretAsync($"https://{keyVaultName}.vault.azure.net/secrets/{secret}")
+                    .GetSecretAsync($"https://{keyVaultName}.vault.azure.net/secrets/{keyVaultSecretName}")
                     .ConfigureAwait(false);
 
                 Console.WriteLine($"Secret: {secret.Value}");
